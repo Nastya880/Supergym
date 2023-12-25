3c8699894ca1945f6d77c1431e14283420946631
Nastya880 <oidmakeevy@gmail.com>
2023-12-25 23:17:48 +0300

    хедер - изменения при переполнении, + начаты табы-абонементы

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {showVideo} from './modules/showVideo';
+import {initTabs} from './modules/tabs/init-tabs';
 
 // ---------------------------------
 
@@ -19,6 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     showVideo();
+    initTabs();
     initModals();
     const form = new Form();
     window.form = form;
