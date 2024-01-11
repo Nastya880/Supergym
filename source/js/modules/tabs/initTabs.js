c7f09313a8c38047bca53b923ef096cfb5e2077a
Nastya880 <oidmakeevy@gmail.com>
2024-01-11 12:35:11 +0300

    табы работют корректно и без js

--- source/js/modules/tabs/initTabs.js
+++ source/js/modules/tabs/initTabs.js
@@ -6,6 +6,9 @@ const initTabs = () => {
   tabs = new Tabs();
   // Используйте в разработке экспортируемую переменную tabs, window сделан для бэкэнда
   window.tabs = tabs;
+
+  const ticketsElement = document.querySelector('[data-tabs="element"');
+  ticketsElement.classList.remove('tickets__element--no-js');
 };
 
 export {initTabs, tabs};
